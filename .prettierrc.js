539cdff1f8c57d31ad7a84d0061416f24fb8cf54
Kit Masaracchia <kit.masaracchia@gmail.com>
2021-07-31 14:42:56 -0600

    Setup linting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  printWidth: 90,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
   arrowParens: 'avoid',
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.jsx', '.js', '.json'],
+        alias: {
+          components: './src/components',
+          navigators: './src/navigators',
+          screens: './src/screens',
+          stores: './src/stores',
+          theme: './src/theme',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
